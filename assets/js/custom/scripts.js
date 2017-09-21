260944aa2b0a96b1fb4a9ef8e27d36499d462044
Anton <suddyadred@gmail.com>
2017-09-21 12:22:35 +0300

    suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -22,25 +22,79 @@ jQuery(document).ready(function($) {
             scrollingSpeed: 700,
             scrollBar: true,
             afterLoad: function(anchorLink, index){
+                //for left menu
+                function clearList() {
+                    var navList = $('.nav-list li');
+                        navList.removeClass('active');
+                }
+                var navList = $('.nav-list li');
+                    navList.each(function (i) {
+                        var item = navList.eq(i),
+                            anchor = item.data('menuanchor');
+                            if(anchor == anchorLink) {
+                                if (!(item.hasClass('active'))) {
+                                    clearList();
+                                    item.addClass('active');
+                                    console.log('Add class active to: '+ anchor);    
+                                }
+                                
+                            }
+                    });
                 //for box-wrap animate
-                console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
                 var box = $('#section' + index + ' .box-wrap');
-                console.log(box);
-                box.each(function (index) {
-                    if (!(box.eq(index).hasClass('show'))) {
-                        box.eq(index).addClass('show');
-                        console.log('show');
-                    }
-                });
+                    box.each(function (i) {
+                        if (!(box.eq(i).hasClass('show'))) {
+                            box.eq(i).addClass('show');
+                        } else {
+                        }
+                    });
+
             }
         });
+
+        //for left menu
         $('.nav-list a').on('click', function () {
             var list = $(this).parents('.nav-list').find('li');
-            list.removeClass('active');
-            $(this).parent().addClass('active');
+                list.removeClass('active');
+                $(this).parent().addClass('active');
         })
     }
-    
-    //for valentine animate
+
+    //for input file
+    $('input[name=files]').on('change', function () {
+        var names = [],
+            count = $(this).get(0).files.length,
+            fileParent =  $(this).parents('.custom-form'),
+            fileBox = fileParent.find('.file-list-box'),
+            fileCount = fileParent.find('.file-count'),
+            fileList =fileParent.find('.file-list');
+
+            if (count > 0) {
+                if (!(fileBox.hasClass('show'))) {
+                    fileBox.addClass('show');
+                }
+                fileList.empty();
+                fileCount.text(count);
+
+                for (var i = 0; i < count; ++i) {
+                    fileList.append('<li>'+ $(this).get(0).files[i].name +'</li>')
+                }
+            } else {
+                if (fileBox.hasClass('show')) {
+                    fileBox.removeClass('show');
+                }
+            }
+        // for (var i = 0; i < count; ++i) {
+        //     names.push($(this).get(0).files[i].name);
+        // }
+        // for (var i = 0; i < $(this).get(0).files.length; ++i) {
+        //     fileList.add('<li>'+ $(this).get(0).files[i].name +'</li>');
+        // }
+        // console.log(names);
+        // $('input[name=file]').val(names);
+        // names.each(function () {
+        //     console.log($(this));
+        // })
+    })
 
 });
